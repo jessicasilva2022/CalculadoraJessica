257debc77a145023a33d5d16ab13630fc061c021
jessicasilva2022 <102984160+jessicasilva2022@users.noreply.github.com>
2023-03-22 15:10:44 -0300

    first commit

---
+++ calculadorabyJess.js
@@ -0,0 +1,11 @@
+function calcular(){
+    let camp1 = window.document.getElementById('numero1')
+    let camp2 = window.document.getElementById('numero2')
+    let resul = window.document.getElementById('resul')
+
+    let num1 = Number(camp1.value)
+    let num2 = Number(camp2.value)
+    let resp = num1 * num2;
+    resul.innerText = ` ${num1} x ${num2} = ${resp}`
+
+  }
